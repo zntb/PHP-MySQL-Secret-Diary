aa756d19e4cd22dfd56165af99c35e50c54c2d6a
zntb <zenetibi@yahoo.com>
2023-10-28 18:38:19 +0300

    Add index.php, set signup and sogin forms

---
+++ index.php
@@ -0,0 +1,19 @@
+<!-- sign up form -->
+<form method="post">
+    <input type="email" name="email" placeholder="Your email">
+    <input type="password" name="password" placeholder="Password">
+    <input type="checkbox" name="stayLoggedIn" value="1">
+    <input type="hidden" name="signUp" value="1">
+
+    <input type="submit" name="submit" value="Sign Up!">
+</form>
+
+<!-- log in form -->
+<form method="post">
+    <input type="email" name="email" placeholder="Your email">
+    <input type="password" name="password" placeholder="Password">
+    <input type="checkbox" name="stayLoggedIn" value="1">
+    <input type="hidden" name="signUp" value="0">
+
+    <input type="submit" name="submit" value="Log In">
+</form>
