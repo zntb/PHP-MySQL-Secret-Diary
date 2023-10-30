56b9fedd27e67aa5b45cc1af56a16106474f9e4b
zntb <zenetibi@yahoo.com>
2023-10-30 11:45:44 +0200

    Add styles and jquery to  signup/login forms

--- index.php
+++ index.php
@@ -96,25 +96,107 @@
 
     } //end if the submit exists
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<div id="error"><?php echo $error; ?></div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Secret Diary</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
+   <!-- CSS -->
+    <style>
+        html {
+            background: url("diary.jpg") no-repeat center center fixed;
+            background-size: cover;
+            z-index: -1;
+        }
+
+        body {
+            background: none;
+        }
+        .container {
+            margin-top: 200px;
+            text-align: center;
+            width: 480px;
+        }
+
+        #loginForm {
+            display: none;
+        }
+
+        .toggleForms {
+            font-weight: bold;
+            color: white;
+        }
+
+    </style>
+ </head>   
+
+ <body>
+    <div id="error"><?php echo $error; ?></div>
+<div class="container">
+    <h1>Secret Diary</h1>
 <!-- sign up form -->
-<form method="post">
-    <input type="email" name="email" placeholder="Your email">
-    <input type="password" name="password" placeholder="Password">
-    <input type="checkbox" name="stayLoggedIn" value="1">
-    <input type="hidden" name="signUp" value="1">
+    <form method="post" id="signupForm">
+        <p>Intersted? Sign up now!</p>
+        <fieldset class="form-group">
+            <input type="email" name="email" class="form-control" placeholder="Your email">
+        </fieldset>
+        <fieldset class="form-group">
+            <input type="password" name="password" class="form-control" placeholder="Password">
+        </fieldset>
+        <fieldset class="checkbox">
+            Stay Logged In:
+            <input type="checkbox" name="stayLoggedIn" value="1">
+        </fieldset>
+        <fieldset class="form-group">
+            <input type="hidden" name="signUp" value="1">
+            <input type="submit" name="submit" class="btn btn-success" value="Sign Up!">
+        </fieldset>    
 
-    <input type="submit" name="submit" value="Sign Up!">
-</form>
+        <p><a href="" class="toggleForms">Log In</a></p>
+    </form>
 
-<!-- log in form -->
-<form method="post">
-    <input type="email" name="email" placeholder="Your email">
-    <input type="password" name="password" placeholder="Password">
-    <input type="checkbox" name="stayLoggedIn" value="1">
-    <input type="hidden" name="signUp" value="0">
+    <!-- log in form -->
+    <form method="post" id="loginForm">
+        <p>Log in using your username and password</p>
+        <fieldset class="form-group">
+            <input type="email" name="email" class="form-control" placeholder="Your email">
+        </fieldset>
+        <fieldset class="form-group">
+            <input type="password" name="password" class="form-control" placeholder="Password">
+        </fieldset>
+        <fieldset class="checkbox">
+            Stay Logged In:
+            <input type="checkbox" name="stayLoggedIn" value="1">
+        </fieldset>
+        <fieldset class="form-group">
+            <input type="hidden" name="signUp" value="0">
+            <input type="submit" name="submit" class="btn btn-success" value="Log In">
+        </fieldset>   
+        
+        <p><a href="" class="toggleForms">Sign Up</a></p>
+    </form>
+</div>
 
-    <input type="submit" name="submit" value="Log In">
-</form>
+        
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
+                crossorigin="anonymous"></script>
+
+        <script type="text/javascript">
+            $(".toggleForms").click(function(){
+                $("#signupForm").toggle();
+                $("#loginForm").toggle();
+            })
+        </script>
+
+    </body>
+
+</html>
+
